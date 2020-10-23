7eb99c04424d6e957f756e106d8641f1e0c04ea8
Matthew The <matthew.the@tum.de>
2020-10-23 15:15:59 +0200

    Added hack for css files for svg exports

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  // we need this to directly download CSS files for the SVG image exports
+  chainWebpack: config => {
+    config.module
+      .rule('css-for-svgs')
+      .test(/\.css.prdb$/)
+      .use('file-loader')
+        .loader('file-loader')
+        .options({name : `css/[name]`})
+        .end()
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'TOPAS Explorer'
+        return args
+      })
+  },
   "transpileDependencies": [
     "vuetify"
   ],
