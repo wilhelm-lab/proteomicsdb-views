5649c72d930fb56910f5e42a9bdcf9cb07d023ee
Patroklos Samaras <patroklos.samaras@tum.de>
2021-02-16 09:32:06 +0100

    view for the Protein Expression bodymap and expression bars, with sample information popup

---
+++ src/views/popup/SampleInfoPopUp.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-dialog v-model="dialogModel" @keydown.esc="close" @click:outside="close" max-width="800">
+    <v-card>
+      <v-btn icon @click="close"><v-icon>mdi-close</v-icon></v-btn>
+      <sampleInfoPanel :sampleId="sampleId" :omics="omics" ></sampleInfoPanel>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import sampleInfoPanel from '@/components/SampleInformation'
+export default {
+  components: {
+    sampleInfoPanel
+  },
+  props: {
+    openDialog: {
+      type: Boolean,
+      default: false
+    },
+    sampleId: String,
+    omics: String
+  },
+  computed: {
+  },
+  data: () => ({
+    dialogModel: false
+  }),
+  methods: {
+    close: function(){
+      this.dialogModel = false;
+      this.$emit('closePopUp', null);
+    },
+  },
+  watch: {
+    openDialog: function() {
+      this.dialogModel = this.openDialog
+    }
+  },
+  mounted() {
+    this.dialogModel = this.openDialog
+  }
+}
+</script>
+<style lang="scss">
+</style>
