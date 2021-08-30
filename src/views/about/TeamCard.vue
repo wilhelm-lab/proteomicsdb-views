487f1419e4d736e546e76969b182216eac122499
Julian Müller <julian2.mueller@tum.de>
2021-08-30 13:23:10 +0200

    Extend about us page

--- src/views/about/TeamCard.vue
+++ src/views/about/TeamCard.vue
@@ -1,40 +1,51 @@
 <template>
   <v-card flat>
     <div class="d-flex flex-no-wrap">
-      <v-avatar
-         class="ma-3 elevation-2"
-         size="120"
-         >
-         <v-img v-if="avatar !== ''" :src="require(`@/assets/people/${avatar}`)"></v-img>
+      <v-avatar class="ma-3 elevation-2" size="120">
+        <v-img
+          v-if="avatar !== ''"
+          :src="require(`@/assets/people/${avatar}`)"
+        ></v-img>
         <v-img v-else :src="require(`@/assets/commons/unknown.png`)"></v-img>
       </v-avatar>
-      <div >
-        <v-card-title
-                class="headline"
-                >{{name}}
-                <div v-for="(icon, i) in social" :key="i">
-                <v-tooltip top>
-                  <template v-slot:activator="{ on }">
-                    <v-icon 
-                               @click="openLink(icon.link)"
-                               class="mx-1"
-                               v-on="on"
-                               >{{icon.icon}}</v-icon>
-                  </template>
-          <span>{{icon.social}}</span>
-                </v-tooltip>
-                </div>
+      <div>
+        <v-card-title class="headline"
+          >{{ name }}
+          <div v-for="(icon, i) in social" :key="i">
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-icon @click="openLink(icon.link)" class="mx-1" v-on="on">{{
+                  icon.icon
+                }}</v-icon>
+              </template>
+              <span>{{ icon.social }}</span>
+            </v-tooltip>
+          </div>
         </v-card-title>
-        <v-card-subtitle class="pb-0">FOCUS - {{focus}}</v-card-subtitle>
+        <v-card-subtitle class="pb-0">FOCUS - {{ focus }}</v-card-subtitle>
         <v-list dense class="py-0">
           <v-list-item>
             <v-list-item-icon class="mx-2">
-              <v-icon>
-                mdi-briefcase
-              </v-icon>
+              <v-icon> mdi-briefcase </v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              {{role}}
+              {{ role }}
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item v-if="affiliation">
+            <v-list-item-icon class="mx-2">
+              <v-icon> mdi-home </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              {{ affiliation }}
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item v-if="position">
+            <v-list-item-icon class="mx-2">
+              <v-icon> mdi-account </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              {{ position }}
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -49,25 +60,22 @@ export default {
     avatar: String,
     name: String,
     role: String,
+    affiliation: String,
+    position: String,
     focus: String,
-    social: Array
+    social: Array,
   },
-  components: {
-  },
-  data: () => ({
-  }),
+  components: {},
+  data: () => ({}),
   methods: {
-    openLink: function(url) {
-      window.open(url,'_blank');
-    }
+    openLink: function (url) {
+      window.open(url, "_blank");
+    },
   },
-  computed: {
-  },
-  watch: {
-  },
-  mounted() {
-  }
-}
+  computed: {},
+  watch: {},
+  mounted() {},
+};
 </script>
 <style lang="scss">
 </style>
